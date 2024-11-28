8e3728d38405e51403c4b6649321fe8e931bf246
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-28 19:57:24 -0300

    chore: changed namespace to file-scoped for code standardization

--- ViewModels/CreateEmployeeViewModel.cs
+++ ViewModels/CreateEmployeeViewModel.cs
@@ -1,25 +1,24 @@
 using Flunt.Notifications;
 using Flunt.Validations;
 
-namespace AppointmentApplication.ViewModels
-{
-    public class CreateEmployeeViewModel : Notifiable<Notification>
-    {
-        public string FirstName { get; set; } = string.Empty!;
-        public string LastName { get; set; } = string.Empty!;
-        public string Email { get; set; } = string.Empty!;
-        public string Profession { get; set; } = string.Empty!;
+namespace AppointmentApplication.ViewModels;
 
-        public Employee MapTo()
-        {
-            AddNotifications(new Contract<Notification>()
-                .Requires()
-                .IsNotNull(FirstName, "Primeiro nome é um campo  obrigatório")
-                .IsNotNull(LastName, "Sobrenome é um campo  obrigatório")
-                .IsNotNull(Email, "Email é um campo  obrigatório")
-                .IsNotNull(Profession, "Profissão é um campo obrigatório"));
+public class CreateEmployeeViewModel : Notifiable<Notification>
+{
+    public string FirstName { get; set; } = string.Empty!;
+    public string LastName { get; set; } = string.Empty!;
+    public string Email { get; set; } = string.Empty!;
+    public string Profession { get; set; } = string.Empty!;
+
+    public Employee MapTo()
+    {
+        AddNotifications(new Contract<Notification>()
+            .Requires()
+            .IsNotNull(FirstName, "Primeiro nome é um campo  obrigatório")
+            .IsNotNull(LastName, "Sobrenome é um campo  obrigatório")
+            .IsNotNull(Email, "Email é um campo  obrigatório")
+            .IsNotNull(Profession, "Profissão é um campo obrigatório"));
             
-            return new Employee(Guid.NewGuid(), FirstName, LastName, Email, Profession);
-        }
+        return new Employee(Guid.NewGuid(), FirstName, LastName, Email, Profession);
     }
 }
