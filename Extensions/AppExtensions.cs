728188312b4b40a040c34a5ceca0d2c3faebfa84
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-27 21:53:36 -0300

    refactor: mapped all app extensions to Extensions folder for better code organization

---
+++ Extensions/AppExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace AppointmentApplication.Extensions;
+
+public static class AppExtensions
+{
+    public static WebApplication AddApplications(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        return app;
+    }
+}
---
+++ Extensions/BuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using AppointmentApplication.Data;
+
+namespace AppointmentApplication.Extensions;
+
+public static class BuilderExtensions
+{
+    public static WebApplicationBuilder AddArchitectures(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        return builder;
+    }
+}
