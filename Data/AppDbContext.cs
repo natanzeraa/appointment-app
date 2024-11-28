15899b83e2ad396b783f1a80a9b3fb3781d655a2
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-28 20:55:22 -0300

    chore: add costumer to AppDbContext.cs file for registering Costumer model into the application

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,10 +1,12 @@
+using AppointmentApplication.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace AppointmentApplication.Data
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<Employee> Employees { get; set; }
+        public required DbSet<Employee> Employees { get; set; }
+        public required DbSet<Costumer> Costumers { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite("DataSource=app.db;Cache=Shared");
