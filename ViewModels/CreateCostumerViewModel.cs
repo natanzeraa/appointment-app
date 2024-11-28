094b403ec7c3dd048873eadbd1af9c90e0879e58
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-28 20:50:23 -0300

    feat: add CreateCostumerViewModel.cs for handling validations on Costumer model

---
+++ ViewModels/CreateCostumerViewModel.cs
@@ -0,0 +1,26 @@
+﻿using AppointmentApplication.Models;
+using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace AppointmentApplication.ViewModels;
+
+public class CreateCostumerViewModel : Notifiable<Notification>
+{
+    public string Name { get; set; } = string.Empty!;
+    public string PhoneNumber { get; set; } = string.Empty!;
+
+    public Costumer MapTo(Func<string, bool> phoneNumberExists)
+    {
+        AddNotifications(new Contract<Notification>()
+            .Requires()
+            .IsNotNull(Name, "Nome é um campo obrigatório")
+            .IsNotNull(PhoneNumber, "Telefone é um campo obrigatório")
+            .Matches(PhoneNumber, @"^\+55\s\(\d{2}\)\s\d{4}-\d{4}$", "Número de telefone deve seguir o formato +99 (99) 9999-9999")
+        );
+
+        if (phoneNumberExists(PhoneNumber))
+            AddNotification(PhoneNumber, "Este número de telefone já está em uso");
+
+        return new Costumer(Guid.NewGuid(), Name, PhoneNumber);
+    }
+}
