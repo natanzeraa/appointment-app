52aa613fa8be0ca84b427eb4e76af986a9851e84
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-29 16:05:56 -0300

    chore: add Appointment model mapping and relations to AppDbContext.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,9 +8,29 @@ namespace AppointmentApplication.Data
         public required DbSet<Employee> Employees { get; set; }
         public required DbSet<Costumer> Costumers { get; set; }
         public required DbSet<Service> Services { get; set; }
-
+        public required DbSet<Appointment> Appointments { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite("DataSource=app.db;Cache=Shared");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Appointment>()
+                .HasOne(a => a.Employee)
+                .WithMany(e => e.Appointments)
+                .HasForeignKey(a => a.EmployeeId);
+
+            modelBuilder.Entity<Appointment>()
+                .HasOne(a => a.Costumer)
+                .WithMany()
+                .HasForeignKey(a => a.CostumerId);
+
+            modelBuilder.Entity<Appointment>()
+                .HasOne(a => a.Service)
+                .WithMany()
+                .HasForeignKey(a => a.ServiceId);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
