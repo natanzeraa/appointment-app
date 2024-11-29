767e67bbf1e02641db7284c1aa21949a9cc2344c
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-29 09:32:55 -0300

    feat: add CreateServiceViewModel.cs for handling validations on Service database objects

---
+++ ViewModels/CreateServiceViewModel.cs
@@ -0,0 +1,23 @@
+﻿using AppointmentApplication.Models;
+using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace AppointmentApplication.ViewModels;
+
+public class CreateServiceViewModel : Notifiable<Notification>
+{
+    public string Name { get; set; } = string.Empty!;
+    public string Description { get; set; } = string.Empty!;
+    public decimal Price { get; set; } = decimal.Zero;
+
+    public Service MapTo()
+    {
+        AddNotifications(new Contract<Notification>()
+            .Requires()
+            .IsNotNull(Name, "Nome é obrigatório")
+            .IsNotNull(Price, "Preço é obrigatório")
+        );
+
+        return new Service(Guid.NewGuid(), Name, Description, Price);
+    }
+}
