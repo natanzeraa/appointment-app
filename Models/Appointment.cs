5af7851db02e1d92685d7408c93cffdc0b29ef47
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-29 16:04:55 -0300

    feat: add appointment record for handling Appointment database model objects

---
+++ Models/Appointment.cs
@@ -0,0 +1,16 @@
+﻿namespace AppointmentApplication.Models;
+
+public record Appointment
+(
+    Guid Id,
+    DateTime Start,
+    DateTime End,
+    Guid EmployeeId,
+    Guid CostumerId,
+    Guid ServiceId
+)
+{
+    public Employee? Employee { get; init; }
+    public Costumer? Costumer { get; init; }
+    public Service? Service { get; init; }
+}
